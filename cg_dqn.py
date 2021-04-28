bf1d3a78dfccd4cb66152e4ae4704606ab154340
patrick <phesse001@csbsju.edu>
2021-04-28 13:15:21 -0500

    first modifications to use with eval_llvm_instcount_policy

--- cg_dqn.py
+++ cg_dqn.py
@@ -2,78 +2,38 @@ import gym
 import compiler_gym
 from dqn import Agent
 import numpy as np
-import matplotlib.pyplot as plt
 import torch
 
-# envs -> compiler_gym.COMPILER_GYM_ENVS
-
-# Compiler = LLVM | Observation Type = Autophase | Reward Signal = IR Instruction count relative to -Oz
-
-env = gym.make("llvm-autophase-ic-v0")
-
-# Use existing dqn to make better decisions
-agent = Agent(gamma = 0.99, epsilon = 1.0, batch_size = 32,
+def train(env):
+    # try running using cbench dataset
+    env.require_datasets(['cBench-v0'])
+    agent = Agent(gamma = 0.99, epsilon = 1.0, batch_size = 32,
             n_actions = env.action_space.n, eps_end = 0.05, input_dims = [56 + int(env.action_space.n)], alpha = 0.005)
 
-# download existing dataset of programs/benchmarks
-env.require_datasets(['cBench-v0'])
+    for i in range(1,15001):
+	    #observation is the 56 dimensional static feature vector from autophase
+	    observation = env.reset()
+	    #maybe try setting done to true every time code size increases
+	    done = False
+	    total = 0
+	    actions_taken = 0
+	    agent.actions_taken = []
+	    change_count = 0
+	    while done == False and actions_taken < 100 and change_count < 10:
+	    	 #only apply finite number of actions to given program
+	        action = agent.choose_action(observation)
+	        #add to previous actions
+	        new_observation, reward, done, info = env.step(action)
+	        #concatenate previous actions vector with autophase
+	        actions_taken += 1
+	        #check total to allow for sequence of actions
+	        total += reward
+	        if reward == 0:
+	            change_count += 1
+	        else:
+	            change_count = 0
+	        #might be more useful to only store memory's of transitions where there was an effect(good or bad)
+	        agent.store_transition(action, observation, reward, new_observation, done)
+	        agent.learn()
+	        observation = new_observation
 
-# action space is described by env.action_space
-# the observation space (autophase) is a 56 dimensional vector
-
-# env.reset() must be called to initialize environment and create initial observation of said env
-tmp = 0
-for i in range(1,15001):
-	#observation is the 56 dimensional static feature vector from autophase
-    observation = env.reset()
-    #number of elements from autophase feature
-    num_actions = env.action_space.n
-    previous_actions = np.zeros((num_actions,))
-    previous_actions = previous_actions.astype('int64')
-    observation = np.concatenate((observation,previous_actions))
-    #maybe try setting done to true every time code size increases
-    done = False
-    total = 0
-    actions_taken = 0
-    agent.actions_taken = []
-    # collect data for visualization
-    iterations = []
-    avg_total = []
-
-    change_count = 0
-    while done == False and actions_taken < 100 and change_count < 10:
-    	  #only apply finite number of actions to given program
-        action = agent.choose_action(observation)
-        #add to previous actions
-        previous_actions[action] += 1
-        new_observation, reward, done, info = env.step(action)
-        #concatenate previous actions vector with autophase
-        new_observation = np.concatenate((new_observation,previous_actions))
-        actions_taken += 1
-        #check total to allow for sequence of actions
-        total += reward
-        if reward == 0:
-            change_count += 1
-        else:
-            change_count = 0
-        #might be more useful to only store memory's of transitions where there was an effect(good or bad)
-        agent.store_transition(action, observation, reward, new_observation, done)
-        agent.learn()
-        observation = new_observation
-        print("Step " + str(i) + " Cumulative Total " + str(total) +
-              " Epsilon " + str(agent.epsilon) + " Action " + str(action) + 
-              " No Effect " + str(info))
-    tmp += total
-    print("avg is " + str(tmp/i))
-    avg_total.append(tmp/i)
-    iterations.append(i)
-    
-plt.scatter(iterations,avg_total)
-plt.savefig("dqn_avg_tot.png")
-# env.commandline() will write the opt command equivalent to the sequence of transformations made by agent
-# print(env.commandline())
-# save the model for future reference
-# env.write_bitcode("./cg_autotuned_program.bc")
-PATH = './cg_dqn.pth'
-torch.save(agent.Q_eval.state_dict(), PATH)
-env.close()
