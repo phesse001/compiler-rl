32d55463a4de39fc7dce21f6f622c121e6a940ca
patrick <phesse001@csbsju.edu>
2021-05-18 11:34:38 -0500

    refactoring to work with bazel and compiler gym leaderboard

--- cg_dqn.py
+++ cg_dqn.py
@@ -2,14 +2,12 @@ import gym
 import compiler_gym
 from dqn import Agent
 import numpy as np
+import torch
 
-def train(env):
+def train(agent, env):
     # try running using cbench dataset
     env.require_datasets(['cBench-v0'])
-    agent = Agent(gamma = 0.99, epsilon = 1.0, batch_size = 32,
-            n_actions = env.action_space.n, eps_end = 0.05, input_dims = [56 + int(env.action_space.n)], alpha = 0.005)
-
-    for i in range(1,15001):
+    for i in range(1,10001):
 	    #observation is the 56 dimensional static feature vector from autophase
 	    observation = env.reset()
 	    #maybe try setting done to true every time code size increases
@@ -23,7 +21,7 @@ def train(env):
 	        action = agent.choose_action(observation)
 	        #add to previous actions
 	        new_observation, reward, done, info = env.step(action)
-	        #concatenate previous actions vector with autophase
+
 	        actions_taken += 1
 	        #check total to allow for sequence of actions
 	        total += reward
@@ -34,4 +32,32 @@ def train(env):
 	        #might be more useful to only store memory's of transitions where there was an effect(good or bad)
 	        agent.store_transition(action, observation, reward, new_observation, done)
 	        agent.learn()
+	        observation = new_observation
+
+def run(agent, env):
+	for i in range(1,101):
+	    #observation is the 56 dimensional static feature vector from autophase
+	    observation = env.reset()
+	    #maybe try setting done to true every time code size increases
+	    done = False
+	    total = 0
+	    actions_taken = 0
+	    agent.actions_taken = []
+	    change_count = 0
+	    while done == False and actions_taken < 100 and change_count < 10:
+	    	 #only apply finite number of actions to given program
+	        action = agent.choose_action(observation)
+	        #add to previous actions
+	        new_observation, reward, done, info = env.step(action)
+
+	        print("Step : " + str(i) + " Episode Reward: " + str(total))
+
+	        actions_taken += 1
+	        #check total to allow for sequence of actions
+	        total += reward
+	        if reward == 0:
+	            change_count += 1
+	        else:
+	            change_count = 0
+
 	        observation = new_observation
