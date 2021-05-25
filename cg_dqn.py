741d03edfff53ccc89d6e615b6ce9a6dcb2dfc2f
patrick <phesse001@csbsju.edu>
2021-05-25 11:31:02 -0500

    fixed numerous bugs (transition save was saving same observation for new and old, now use included instcount norm (instead of computing myself), renamed variable names, changed a few hyperparameters)

--- cg_dqn.py
+++ cg_dqn.py
@@ -5,24 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 
-def normalize(obv):
-    mean = np.mean(obv)
-    std = np.std(obv)
-    return (obv - mean)/std
-
-
-def equal(a1, a2):
-    diff = []
-    for i in range(len(a1)):
-        if(a1[i] != a2[i]):
-            diff.append(i)
-
-    return diff
-
-observations = np.zeros(280)
+observations = np.zeros(276)
 
 def save_observation(observation):
-    n = 70
+    n = 69
     observations[3*n:4*n] = observations[2*n:3*n]
     observations[2*n:3*n] = observations[n:2*n]
     observations[n:2*n] = observations[0:n]
@@ -44,7 +30,7 @@ def save_observation(observation):
 '''
 
 env = gym.make("llvm-ic-v0")
-env.observation_space = "InstCount"
+env.observation_space = "InstCountNorm"
 
 for d in env.datasets:
     if d.name != "benchmark://cbench-v1":
@@ -56,28 +42,22 @@ for benchmark in env.datasets.benchmarks():
     dataset.append(benchmark)
 
 
-agent = Agent(gamma = 0.99, epsilon = 1.0, batch_size = 32,
-            n_actions = env.action_space.n, eps_end = 0.1, input_dims = [280], alpha = 0.005)
-
-# download existing dataset of programs/benchmarks
+agent = Agent(gamma = 0.95, epsilon = 1.0, batch_size = 32,
+            n_actions = env.action_space.n, eps_end = 0.05, input_dims = [276], alpha = 0.005)
 
 action_space = env.action_space.names
 
-tmp = 0
-# collect data for visualization
+total = 0
 iterations = []
 avg_total = []
-for i in range(1,50001):
-	#observation is the 56 dimensional static feature vector from autophase
+
+for i in range(1,100001):
     observation = env.reset(benchmark = dataset[np.random.choice(len(dataset))])
     print(env.benchmark)
-    # Let's normalize the input vector...
-    observation = normalize(observation)
-
     save_observation(observation)
 
     done = False
-    total = 0
+    episode_total = 0
     actions_taken = 0
     agent.actions_taken = []
     change_count = 0
@@ -86,31 +66,28 @@ for i in range(1,50001):
 
         action = agent.choose_action(observations)
         new_observation, reward, done, info = env.step(action)
-        new_observation = normalize(new_observation)
-        save_observation(new_observation)
-
         # save old observation to be used for store_transition
         old_observations = observations
+        save_observation(new_observation)
 
         actions_taken += 1
 
         #check total to allow for sequence of actions
-        total += reward
+        episode_total += reward
         if reward == 0:
             change_count += 1
         else:
             change_count = 0
-
-        # TODO - fix so new observation is saved: right now it is just a 70d vector but should be 280
+        
         agent.store_transition(action, old_observations, reward, observations, done)
         agent.learn()
 
-        print("Step: " + str(i) + " Episode Total: " + "{:.4f}".format(total) +
+        print("Step: " + str(i) + " Episode Total: " + "{:.4f}".format(episode_total) +
               " Epsilon: " + "{:.4f}".format(agent.epsilon) + " Action: " + str(action_space[action]))
     
-    tmp += total
-    print("Average Episode Reward: " + str(tmp/i))
-    avg_total.append(tmp/i)
+    total += episode_total
+    print("Average Episode Reward: " + str(total/i))
+    avg_total.append(total/i)
     iterations.append(i)
     
 plt.scatter(iterations,avg_total)
