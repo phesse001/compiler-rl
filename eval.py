ad8df73d86d462752b08d1eae4fb45108f5d0bbd
Patrick <phesse001@csbsju.edu>
2021-06-07 20:21:26 -0500

    updates

--- eval.py
+++ eval.py
@@ -7,15 +7,9 @@ from compiler_gym.envs import LlvmEnv
 from dqn import rollout,Agent
 import torch
 
-# load the model 
-
-env = gym.make("llvm-ic-v0")
-
-env.observation_space = "InstCountNorm"
-
 def run(env: LlvmEnv) -> None:
-    
-    agent = Agent(n_actions = env.action_space.n, input_dims = [69])
+
+    agent = Agent(n_actions = 15, input_dims = [69])
 
     agent.Q_eval.load_state_dict(torch.load("./dqn.pth"))
 
