060209bf6b8e2eac587bae90b899c48d95dd114f
patrick <phesse001@csbsju.edu>
2021-06-14 15:43:08 -0500

    updates

--- train.py
+++ train.py
@@ -2,11 +2,9 @@ from dqn import train, Agent
 import gym
 import compiler_gym
 from absl import app
-import torch
 
 if __name__ == "__main__":
     env = gym.make("llvm-ic-v0")
     agent = Agent(input_dims = [69], n_actions = 15)
     train(agent, env)
-    PATH = './H10-N4000-INSTCOUNTNORM-CBENCH.pth'
-    torch.save(agent.Q_eval.state_dict(), PATH)
+    
