2e41f682f44f332b776a24fa0bfdef681a94bf1d
Patrick <phesse001@csbsju.edu>
2021-08-09 17:26:00 -0500

    initial refactor

--- cg-rllib.py
+++ cg-rllib.py
@@ -67,4 +67,52 @@ config['env'] = 'compiler_gym'
 config['model']['fcnet_activation'] = 'relu'
 config['model']['fcnet_hiddens'] = [1024, 1024, 1024]
 
+# train, load, and test functions from https://bleepcoder.com/ray/644594660/rllib-best-workflow-to-train-save-and-test-agent
+def train(stop_criteria):
 
+    """
+    Train an RLlib PPO agent using tune until any of the configured stopping criteria is met.
+    :param stop_criteria: Dict with stopping criteria.
+        See https://docs.ray.io/en/latest/tune/api_docs/execution.html#tune-run
+    :return: Return the path to the saved agent (checkpoint) and tune's ExperimentAnalysis object
+        See https://docs.ray.io/en/latest/tune/api_docs/analysis.html#experimentanalysis-tune-experimentanalysis
+    """
+    analysis = ray.tune.run(ppo.PPOTrainer, config=config, local_dir=save_dir, stop=stop_criteria,
+                            checkpoint_at_end=True)
+    # list of lists: one list per checkpoint; each checkpoint list contains 1st the path, 2nd the metric value
+    checkpoints = analysis.get_trial_checkpoints_paths(trial=analysis.get_best_trial('episode_reward_mean'),
+                                                       metric='episode_reward_mean')
+    # retriev the checkpoint path; we only have a single checkpoint, so take the first one
+    checkpoint_path = checkpoints[0][0]
+    return checkpoint_path, analysis
+
+def load(path):
+    """
+    Load a trained RLlib agent from the specified path. Call this before testing a trained agent.
+    :param path: Path pointing to the agent's saved checkpoint (only used for RLlib agents)
+    """
+    agent = ppo.PPOTrainer(config=config, env=make_training_env())
+    agent.restore(path)
+    return agent
+
+def test(agent):
+    """Test trained agent for a single episode. Return the episode reward"""
+    # instantiate env class
+    with make_training_env() as env:
+
+      # run until episode ends
+      episode_reward = 0
+      done = False
+      obs = env.reset()
+      while not done:
+          action = agent.compute_action(obs)
+          obs, reward, done, info = env.step(action)
+          episode_reward += reward
+
+    return episode_reward
+
+# start training
+
+agent_path,anaysis_obj = train(stop={"episodes_total":"200000"})
+test_agent = load(agent_path)
+cumulative_reward = test(test_agent)
